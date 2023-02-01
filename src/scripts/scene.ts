9c9d952a585f9207c9b3059bc55d50561609fc4b
Willian Louza <willianlouza@gmail.com>
2023-02-01 13:10:16 -0300

    add infinity snow particle

--- src/scripts/scene.ts
+++ src/scripts/scene.ts
@@ -13,26 +13,27 @@ let camera_2: THREE.PerspectiveCamera;
 let scene_1: THREE.Scene;
 let scene_2: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
+let controls: OrbitControls;
 
 let book: THREE.Group;
-let controls: OrbitControls;
+let snows: SnowFlake[] = [];
 
 window.addEventListener("resize", onResize);
 
 function init() {
   scene_1 = new THREE.Scene();
   scene_1.fog = new THREE.FogExp2(0x000000, 0.0008);
-  
+
   scene_2 = new THREE.Scene();
   scene_2.fog = new THREE.FogExp2(0x000000, 0.0008);
 
   camera_1 = new THREE.PerspectiveCamera(75, ASPECT, 0.1, 1000);
   camera_1.position.set(0, 0, 15);
   camera_1.lookAt(scene_1.position);
-  
+
   camera_2 = new THREE.PerspectiveCamera(75, ASPECT, 0.1, 1000);
   camera_2.position.set(0, 0, 15);
-  camera_2.lookAt(scene_1.position);
+  camera_2.lookAt(scene_2.position);
 
   renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -40,11 +41,14 @@ function init() {
   domScene?.appendChild(renderer.domElement);
 
   const light = new THREE.AmbientLight(0xffffff, 1);
-  light.position.set(0, 500, 0);
   scene_1.add(light);
 
+  const light2 = new THREE.DirectionalLight(0xffffff, 50);
+  light2.position.set(0, 0, 500);
+  scene_2.add(light2);
+
   loadBook();
-  loadParticles()
+  loadParticles();
 
   controls = new OrbitControls(camera_1, renderer.domElement);
   controls.autoRotate = false;
@@ -84,40 +88,69 @@ function loadBook() {
 }
 
 function beforeUpdate() {
+  if (!book) return;
   let y = book.rotation.y;
   book.rotation.set(0, y + 0.005, 0);
+
+  for (let i = 0; i < snows.length; i++) {
+    const ref = snows[i];
+    ref.movePosition(1, 1);
+    if (ref.position.y < -50) {
+      ref.reset();
+    }
+  }
 
   controls.update();
 }
 
 function loadParticles() {
-  const geometry = new THREE.BufferGeometry();
-  const vertices: any = [];
+  const geometry = new THREE.CircleGeometry();
+  const material = new THREE.MeshPhongMaterial({ color: 0xffff });
+  material.transparent = true;
+  material.opacity = 0.5;
 
-  const loader = new THREE.TextureLoader();
-
-  const sprite = loader.load("/images/circle.png");
+  const points: any[] = [];
 
   for (let i = 0; i < 5000; i++) {
-    const x = Math.random() * 500 - 250;
-    const y = Math.random() * 500 - 250;
-    const z = 0
+    const x = Math.random() * 100 - 50;
+    const y = Math.random() * 100 - 50;
+    const z = -10;
+    const scale = Math.random() * (0.1 - 0.01) + 0.01;
 
-    vertices.push(x, y, z);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(scale, scale, scale);
+    mesh.position.set(x, y, z);
+
+    points.push(mesh);
+    snows.push(new SnowFlake(mesh.position, mesh.scale));
   }
 
-  geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+  scene_2.add(...points);
+}
 
-  const material = new THREE.PointsMaterial({
-    size: 0.2,
-    map: sprite,
-    blending: THREE.AdditiveBlending,
-    depthTest: false,
-    transparent: true,
-  });
-  const particles = new THREE.Points(geometry, material);
+class SnowFlake {
+  position: THREE.Vector3;
+  scale: THREE.Vector3;
+  speedX: number;
+  speedY: number;
+  constructor(position: THREE.Vector3, scale: THREE.Vector3) {
+    this.position = position;
+    this.scale = scale;
+    this.speedX = this.getRandom(0.05, 0.01);
+    this.speedY = this.getRandom(0.05, 0.01);
+  }
 
-  scene_2.add(particles);
+  movePosition(x: number, y: number) {
+    this.position.set(this.position.x - x * this.speedX, this.position.y - y * this.speedY, this.position.z);
+  }
+
+  getRandom(max: number, min: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  reset() {
+    this.position.set(Math.random() * 100 - 50, 50, 0);
+  }
 }
 
 export default function initialize() {
