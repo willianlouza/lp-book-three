a1bc5c1cfb05afb1f2f3ecc2da2d77693d49ea4f
Willian Louza <willianlouza@gmail.com>
2023-02-03 10:23:14 -0300

    pre deploy

--- src/scripts/scene.ts
+++ src/scripts/scene.ts
@@ -1,20 +1,27 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const domScene = document.getElementById("three-scene");
+const snowContainer = document.getElementById("snow-scene");
+const bookContainer = document.getElementById("book-scene");
 
 let SCREEN_WIDTH = window.innerWidth;
 let SCREEN_HEIGHT = window.innerHeight;
 let ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
 
+let isMobile = SCREEN_WIDTH < 1060;
+
 let book_camera: THREE.PerspectiveCamera;
 let snow_camera: THREE.PerspectiveCamera;
 let book_scene: THREE.Scene;
 let snow_scene: THREE.Scene;
-let renderer: THREE.WebGLRenderer;
+let snowRenderer: THREE.WebGLRenderer;
+let bookRenderer: THREE.WebGLRenderer;
 
 let book: THREE.Group;
 let snows: SnowFlake[] = [];
+
+let isMoving = false;
+let lastPosition = 0;
 
 window.addEventListener("resize", onResize);
 
@@ -25,18 +32,33 @@ function init() {
   snow_scene = new THREE.Scene();
   snow_scene.fog = new THREE.FogExp2(0x000000, 0.0008);
 
-  book_camera = new THREE.PerspectiveCamera(75, ASPECT, 0.1, 1000);
-  book_camera.position.set(0, 0, 15);
+  book_camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+  book_camera.position.set(0, 0, 12);
   book_camera.lookAt(snow_scene.position);
 
   snow_camera = new THREE.PerspectiveCamera(75, ASPECT, 0.1, 1000);
-  snow_camera.position.set(0, 0, 15);
+  snow_camera.position.set(0, 0, 30);
   snow_camera.lookAt(snow_scene.position);
 
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-  renderer.autoClear = false;
-  domScene?.appendChild(renderer.domElement);
+  snowRenderer = new THREE.WebGLRenderer({ alpha: true });
+  snowRenderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  snowRenderer.autoClear = false;
+
+  bookRenderer = new THREE.WebGLRenderer({ alpha: true });
+  if (isMobile) {
+    bookRenderer.setSize(400, 400);
+  } else {
+    bookRenderer.setSize(600, 600);
+  }
+  bookRenderer.autoClear = false;
+
+  snowContainer?.appendChild(snowRenderer.domElement);
+  bookContainer?.appendChild(bookRenderer.domElement);
+
+  bookRenderer.domElement.addEventListener("mousedown", onMouseDown, true);
+  bookRenderer.domElement.addEventListener("mouseup", onMouseUp, true);
+  bookRenderer.domElement.addEventListener("mousemove", onMouseMove, true);
+  bookRenderer.domElement.addEventListener("mouseleave", onMouseUp, true);
 
   const light = new THREE.AmbientLight(0xffffff, 1);
   book_scene.add(light);
@@ -47,12 +69,14 @@ function init() {
 
   loadBook();
   loadParticles();
-
+}
+function beforeUpdate() {
+  snowFall();
 }
 function render() {
   beforeUpdate();
-  renderer.render(book_scene, book_camera);
-  renderer.render(snow_scene, snow_camera);
+  bookRenderer.render(book_scene, book_camera);
+  snowRenderer.render(snow_scene, snow_camera);
 }
 function animate() {
   requestAnimationFrame(animate);
@@ -62,36 +86,52 @@ function onResize() {
   SCREEN_WIDTH = window.innerWidth;
   SCREEN_HEIGHT = window.innerHeight;
   ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+  isMobile = SCREEN_WIDTH < 1060;
   book_camera.aspect = ASPECT;
   snow_camera.aspect = ASPECT;
   book_camera.updateProjectionMatrix();
   snow_camera.updateProjectionMatrix();
-  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  snowRenderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  if (isMobile) {
+    bookRenderer.setSize(400, 400);
+  } else {
+    bookRenderer.setSize(600, 600);
+  }
 }
 function loadBook() {
   const loader = new GLTFLoader();
   loader.load("models/gltf/book.glb", (object) => {
-    object.scene.rotation.set(0, 3, 0);
-    object.scene.position.set(10, -6, 0);
+    object.scene.rotation.set(0, 2.5, 0);
+    object.scene.position.set(0, -6, 0);
     book = object.scene;
     book_scene.add(object.scene);
   });
 }
+function onMouseMove(event: MouseEvent) {
+  if (!isMoving) return;
+  const deltaX = event.clientX / window.innerWidth;
+  let direction = 0;
 
-function beforeUpdate() {
-  if (!book) return;
-  let y = book.rotation.y;
-  book.rotation.set(0, y + 0.005, 0);
-
-  for (let i = 0; i < snows.length; i++) {
-    const ref = snows[i];
-    ref.movePosition(1, 1);
-    if (ref.position.y < -50) {
-      ref.reset();
-    }
+  if (deltaX > lastPosition) {
+    lastPosition = deltaX;
+    direction = 1;
+  } else if (deltaX < lastPosition) {
+    lastPosition = deltaX;
+    direction = -1;
   }
-}
 
+  if (lastPosition === direction) {
+    direction = 0;
+  }
+
+  book.rotateY(direction * 0.1);
+}
+function onMouseDown() {
+  isMoving = true;
+}
+function onMouseUp() {
+  isMoving = false;
+}
 function loadParticles() {
   const geometry = new THREE.CircleGeometry();
   const material = new THREE.MeshPhongMaterial({ color: 0xffff });
@@ -117,6 +157,15 @@ function loadParticles() {
   snow_scene.add(...points);
 }
 
+function snowFall() {
+  for (let i = 0; i < snows.length; i++) {
+    const ref = snows[i];
+    ref.movePosition(1, 1);
+    if (ref.position.y < -50) {
+      ref.reset();
+    }
+  }
+}
 class SnowFlake {
   position: THREE.Vector3;
   scale: THREE.Vector3;
